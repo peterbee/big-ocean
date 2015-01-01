9f79a5938dc4f0d398a14016713ae799396a3d08
Pete Schmidt <pschmidt@rabbleandrouser.com>
2014-12-31 20:58:11 -0700

    Use WorldToViewportPoint to calculate screen ratio

--- Assets/Class/CameraClass.js
+++ Assets/Class/CameraClass.js
@@ -4,12 +4,9 @@ public var player : GameObject;
 public var horizon : GameObject;
 
 function FixedUpdate() {
-	var screenPosition = camera.WorldToScreenPoint(player.transform.position);
+	var screenPosition = camera.WorldToViewportPoint(player.transform.position);
 	
-	var ratioX = screenPosition.x/camera.pixelWidth;
-	var ratioY = screenPosition.y/camera.pixelHeight;
-	
-	transform.position.x -= 0.5 - ratioX;
-	transform.position.y -= 0.5 - ratioY;
-	horizon.transform.position.x -= 0.5 - ratioX;
+	transform.position.x -= 0.5 - screenPosition.x;
+	transform.position.y -= 0.5 - screenPosition.y;
+	horizon.transform.position.x -= 0.5 - screenPosition.x;
 }
